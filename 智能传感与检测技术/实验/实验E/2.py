faeb68cc92a7005dd0e7b4ff03af130e378b8d61
“ImCodeKing” <lizhaoya21@mails.tsinghua.edu.cn>
2023-12-12 00:30:45 +0800

    1

--- 智能传感与检测技术/实验/实验E/2.py
+++ 智能传感与检测技术/实验/实验E/2.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # 读取CSV文件
-file_path = 'D:/李昭阳/大学/University_Junior_Fall/智能传感与检测技术/实验/实验E/ECPPG_2023-10-26 lzyPPG.csv'
+file_path = 'D:/大学/University_Junior_Fall/智能传感与检测技术/实验/实验E/ECPPG_2023-10-26 lchPPG.csv'
 # file_path = 'D:/大学/University_Junior_Fall/智能传感与检测技术/实验/实验E/ECPPG_2023-10-26 lzy.csv'
 data = pd.read_csv(file_path)
 
@@ -11,11 +11,11 @@ x_column = ' Sample Count'
 y_column = ' Red Count'
 
 # 截取数据集的一部分
-start_index = (len(data) ) // 2
-end_index = start_index + 100
-# subset_df = data.iloc[start_index:end_index]
-subset_df = data.iloc[0:len(data)//2]
-# subset_df[x_column] = subset_df[x_column] - 99387
+start_index = (len(data) + 1000) // 2
+end_index = start_index + 800
+subset_df = data.iloc[start_index:end_index]
+# subset_df = data.iloc[0:len(data)//2]
+subset_df[x_column] = subset_df[x_column] - 99387
 
 # 绘图
 plt.plot(subset_df[x_column], subset_df[y_column])
